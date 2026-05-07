419a8c956d0ef5b4b887b9c081b192f52980e551
M11409904 <M11409904@mail.ntust.edu.tw>
2026-05-07 19:54:52 +0800

    Update chatbotb.js

--- chatbotb.js
+++ chatbotb.js
@@ -5,7 +5,7 @@ class ChatBot {
         // ****************************************************************
         //this.webhookUrl = 'https://你的帳號.app.n8n.cloud/webhook/chatb';
         //http://localhost:5678/webhook/chatb
-        https://scouts-clerical-lid.ngrok-free.dev//webhook/chatb
+        https://scouts-clerical-lid.ngrok-free.dev/webhook/chatb
         // ****************************************************************        
         // Initialize session ID
         this.sessionId = this.getOrCreateSessionId();
